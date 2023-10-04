346d59dd7f15ca730e8752d086e49ad5e3ed1924
Marius Zaharia <marius@lecampusazure.net>
2023-10-04 10:59:45 +0000

    temporary bypass of the auth message

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -87,8 +87,9 @@ const Chat = () => {
     
     const getUserInfoList = async () => {
         const userInfoList = await getUserInfo();
-        if (userInfoList.length === 0 && window.location.hostname !== "127.0.0.1") {
-            setShowAuthMessage(true);
+        if (userInfoList.length === 0 && 
+            window.location.hostname !== "127.0.0.1") {
+            setShowAuthMessage(false);
         }
         else {
             setShowAuthMessage(false);
